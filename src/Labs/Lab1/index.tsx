2495550a0b732495362a34a9b76d3cd4e14a23ee
Kaden <casanave.k@northeastern.edu>
2024-09-25 15:42:37 -0400

    minor lab1 additions/formatting

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -2,7 +2,6 @@ export default function Lab1() {
     return (
         <div id="wd-lab1">
             <h2>Lab 1</h2>
-
             <h3>HTML Examples</h3>
             <div id="wd-h-tag">
                 <h4>Heading Tags</h4>
@@ -91,43 +90,43 @@ export default function Lab1() {
                         </tr>
                         <tr>
                             <td>Q4</td>
-                            <td>JavaScript</td>
+                            <td>Python</td>
                             <td>2/28/21</td>
                             <td>90</td>
                         </tr>
                         <tr>
                             <td>Q5</td>
-                            <td>JavaScript</td>
+                            <td>Discretemath</td>
                             <td>3/1/21</td>
                             <td>80</td>
                         </tr>
                         <tr>
                             <td>Q6</td>
-                            <td>JavaScript</td>
+                            <td>Leetcode</td>
                             <td>3/13/21</td>
                             <td>100</td>
                         </tr>
                         <tr>
                             <td>Q7</td>
-                            <td>JavaScript</td>
+                            <td>TypeScript</td>
                             <td>3/28/21</td>
                             <td>85</td>
                         </tr>
                         <tr>
                             <td>Q8</td>
-                            <td>JavaScript</td>
+                            <td>AWS</td>
                             <td>4/1/21</td>
                             <td>90</td>
                         </tr>
                         <tr>
                             <td>Q9</td>
-                            <td>JavaScript</td>
+                            <td>SQL</td>
                             <td>4/6/21</td>
                             <td>95</td>
                         </tr>
                         <tr>
                             <td>Q10</td>
-                            <td>JavaScript</td>
+                            <td>Hacking</td>
                             <td>4/20/21</td>
                             <td>100</td>
                         </tr>
@@ -144,16 +143,12 @@ export default function Lab1() {
                 <h4>Image tag</h4>
                 Loading an image from the internet:
                 <br />
-                <img id="wd-starship"
-                    width="400px"
-                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
-                />
+                <img id="wd-starship" width="400px" src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" />
                 <br />
                 Loading a local image:
                 <br />
                 <img id="wd-teslabot" src="images/teslabot.jpg" height="200px" />
             </div>
-
             <div id="wd-forms">
                 <h4>Form Elements</h4>
                 <form id="wd-text-fields">
@@ -161,36 +156,21 @@ export default function Lab1() {
                     <label htmlFor="wd-text-fields-username">Username:</label>
                     <input id="wd-text-fields-username" placeholder="jdoe" /> <br />
                     <label htmlFor="wd-text-fields-password">Password:</label>
-                    <input type="password" id="wd-text-fields-password" value="123@#$asd" />
-                    <br />
+                    <input type="password" id="wd-text-fields-password" value="123@#$asd" /> <br />
                     <label htmlFor="wd-text-fields-first-name">First name:</label>
                     <input type="text" id="wd-text-fields-first-name" title="John" /> <br />
                     <label htmlFor="wd-text-fields-last-name">Last name:</label>
-                    <input type="text" id="wd-text-fields-last-name" placeholder="Doe"
-                        value="Wonderland" title="The last name" />
+                    <input type="text" id="wd-text-fields-last-name" placeholder="Doe" value="Wonderland" title="The last name" />
+
                     <h4>Other HTML field types</h4>
                     <label htmlFor="wd-text-fields-email"> Email: </label>
-                    <input type="email"
-                        placeholder="jdoe@somewhere.com"
-                        id="wd-text-fields-email" /><br />
-
-                    <label htmlFor="wd-text-fields-salary-start"> Starting salary:
-                    </label>
-                    <input type="number"
-                        id="wd-text-fields-salary-start"
-                        placeholder="1000"
-                        value="100000" /><br />
-
+                    <input type="email" placeholder="jdoe@somewhere.com" id="wd-text-fields-email" /><br />
+                    <label htmlFor="wd-text-fields-salary-start"> Starting salary:</label>
+                    <input type="number" id="wd-text-fields-salary-start" placeholder="1000" value="100000" /><br />
                     <label htmlFor="wd-text-fields-rating"> Rating: </label>
-                    <input type="range" id="wd-text-fields-rating"
-                        placeholder="Doe"
-                        max="5"
-                        value="4" /><br />
-
+                    <input type="range" id="wd-text-fields-rating" placeholder="Doe" max="5" value="4" /><br />
                     <label htmlFor="wd-text-fields-dob"> Date of birth: </label>
-                    <input type="date"
-                        id="wd-text-fields-dob"
-                        value="2000-01-21" /><br />
+                    <input type="date" id="wd-text-fields-dob" value="2000-01-21" /><br />
                 </form>
             </div>
 
@@ -198,34 +178,31 @@ export default function Lab1() {
             <label>Biography:</label><br />
             <textarea id="wd-textarea" cols={30} rows={10}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
 
+            <h5 id="wd-buttons">Buttons</h5>
+            <button id="wd-all-good" onClick={() => alert("Life is Good!")} type="button"> Hello World!</button>
+
+            <h5>File upload</h5>
+            <input id="wd-upload" type="file" />
+
             <h5 id="wd-radio-buttons">Radio buttons</h5>
-
             <label>Favorite movie genre:</label><br />
-
             <input type="radio" name="radio-genre" id="wd-radio-comedy" />
             <label htmlFor="wd-radio-comedy">Comedy</label><br />
-
             <input type="radio" name="radio-genre" id="wd-radio-drama" />
             <label htmlFor="wd-radio-drama">Drama</label><br />
-
             <input type="radio" name="radio-genre" id="wd-radio-scifi" />
             <label htmlFor="wd-radio-scifi">Science Fiction</label><br />
-
             <input type="radio" name="radio-genre" id="wd-radio-fantasy" />
             <label htmlFor="wd-radio-fantasy">Fantasy</label>
 
             <h5 id="wd-checkboxes">Checkboxes</h5>
             <label>Favorite movie genre:</label><br />
-
             <input type="checkbox" name="check-genre" id="wd-chkbox-comedy" />
             <label htmlFor="wd-chkbox-comedy">Comedy</label><br />
-
             <input type="checkbox" name="check-genre" id="wd-chkbox-drama" />
             <label htmlFor="wd-chkbox-drama">Drama</label><br />
-
             <input type="checkbox" name="check-genre" id="wd-chkbox-scifi" />
             <label htmlFor="wd-chkbox-scifi">Science Fiction</label><br />
-
             <input type="checkbox" name="check-genre" id="wd-chkbox-fantasy" />
             <label htmlFor="wd-chkbox-fantasy">Fantasy</label>
 
@@ -236,8 +213,7 @@ export default function Lab1() {
             <select id="wd-select-one-genre">
                 <option value="COMEDY">Comedy</option>
                 <option value="DRAMA">Drama</option>
-                <option selected value="SCIFI">
-                    Science Fiction</option>
+                <option selected value="SCIFI">Science Fiction</option>
                 <option value="FANTASY">Fantasy</option>
             </select>
 
@@ -246,20 +222,15 @@ export default function Lab1() {
             <select id="wd-select-many-genre" multiple>
                 <option selected value="COMEDY">Comedy</option>
                 <option value="DRAMA">Drama</option>
-                <option selected value="SCIFI">
-                    Science Fiction</option>
+                <option selected value="SCIFI">Science Fiction</option>
                 <option value="FANTASY">Fantasy</option>
             </select>
 
             <h4>Anchor tag</h4>
-            Please
-            <a id="wd-lipsum" href="https://www.lipsum.com">click here</a>
-            to get dummy text<br />
+            Please <a id="wd-lipsum" href="https://www.lipsum.com">click here</a> to get dummy text<br />
 
             <h4>Anchor tag</h4>
-            Please
-            <a id="wd-github" href="https://github.com/kadencas/kanbas-react-web-app.git">click here</a>
-            to get github<br />
+            Please <a id="wd-github" href="https://github.com/kadencas/kanbas-react-web-app.git">click here</a> to get github<br />
 
         </div>
     );
