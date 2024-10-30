60def61518c670cb7b13b9b556eb6d70de224d05
Kaden <casanave.k@northeastern.edu>
2024-10-30 18:46:05 -0400

    all up to 3.5

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -1,12 +1,25 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 export default function AccountNavigation() {
-    return (
-        <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
-            <Link to={`/Kanbas/Account/Signin`} className="list-group-item active border border-0" > Signin  </Link> <br />
-            <Link to={`/Kanbas/Account/Signup`} className="list-group-item text-danger border border-0" > Signup  </Link> <br />
-            <Link to={`/Kanbas/Account/Profile`} className="list-group-item text-danger border border-0"> Profile </Link> <br />
-        </div>
-    );
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+  const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+  const { pathname } = useLocation();
+
+  return (
+    <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
+      {links.map((link) => (
+        <Link
+          key={link}
+          to={`/Kanbas/Account/${link}`}
+          className={`list-group-item ${pathname.includes(link) ? "active" : ""} border border-0`}
+        >
+          {link}
+        </Link>
+      ))}
+    </div>
+  );
 }
+
 
 
